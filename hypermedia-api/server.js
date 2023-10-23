8006cae9079e219ffe6ba0552a6f0d9075c7c111
Fikayo Adepoju <fikfikky@gmail.com>
2023-10-23 15:55:19 +0100

    File Upload Start

--- hypermedia-api/server.js
+++ hypermedia-api/server.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static('uploads'));
 
 // Configure Multer
 const storage = multer.diskStorage({
