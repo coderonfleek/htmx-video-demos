df75a62f59e4917ad99b1189f4ded977f8c8a767
Fikayo Adepoju <fikfikky@gmail.com>
2023-10-23 15:53:04 +0100

    File Uploads Complete

--- hypermedia-api/server.js
+++ hypermedia-api/server.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static('uploads'));
 
 // Configure Multer
 const storage = multer.diskStorage({
@@ -26,8 +27,15 @@ const upload = multer({storage : storage});
 
 app.post("/upload", upload.single("file"), async (req, res) => {
     const filePath = req.file.path;
+    const body = req.body;
     console.log(filePath);
-    
+    res.send(`
+        <div>
+            <p><b>Email:</b> ${body.myEmail}</p>
+            <p><b>About:</b> ${body.aboutMe}</p>
+            <p><img width="300" height="300" src="http://localhost:1330/${req.file.filename}" /></p>
+        </div>
+    `);
 })
 
 
